1b8368688693bd85d00f994d565025529b263f0c
Krzysztof Ciombor <krzysztof.ciombor@swmansion.com>
2017-09-14 16:02:59 +0200

    Add more tests

--- __tests__/asyncStorageHelper.test.js
+++ __tests__/asyncStorageHelper.test.js
@@ -30,3 +30,48 @@ test('createStore respects false for methods', () => {
   expect(store.removeTest).toBeUndefined()
 })
 
+test('createStore methods work correctly', () => {
+  const store = createStore({
+    test: {
+      get: true,
+      set: true,
+      remove: true,
+    }
+  })
+
+  expect(store.getTest()).resolves.toBeUndefined()
+
+  store.setTest(true)
+  expect(store.getTest()).resolves.toEqual(true)
+
+  store.removeTest()
+  expect(store.getTest()).resolves.toBeUndefined()
+})
+
+test('createStore sets defaults to true', () => {
+  const store = createStore({
+    test,
+  })
+
+  expect(store.getTest).toBeDefined()
+  expect(store.setTest).toBeDefined()
+  expect(store.removeTest).toBeDefined()
+})
+
+test('createStore works for objects', () => {
+  const store = createStore({
+    test,
+  })
+
+  const testObject = {
+    stringProp: 'string',
+    numProp: 123,
+    nested: {
+      foo: 'bar',
+    },
+  }
+  store.setTest(testObject)
+
+  expect(store.getTest()).resolves.toEqual(testObject)
+})
+
